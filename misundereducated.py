6275565794fea68c58fefb2709293ba8a4277097
Kyle Shepherd <kshepherd@winkapp.com>
2017-06-23 21:25:09 -0700

    something broke

--- misundereducated.py
+++ misundereducated.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def misundereducated():
-    return 'Misundereducated'
+    ##return render_template('templates/index.html')
+    ##return render_template('index.html')
+    return "Hello"
